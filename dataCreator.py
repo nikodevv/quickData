3d11b38a6fccb8c870bccbbc840f9fc66e619015
Niko Oshinov <niko.oshinov@gmail.com>
2018-01-06 22:26:24 -0500

    added formatting function to turn string values into int

--- dataCreator.py
+++ dataCreator.py
@@ -9,13 +9,16 @@ class DataScraper():
 	def main(self,company_IS_data_link):
 		self.page = requests.get(company_IS_data_link)
 		self.tree = fromstring(self.page.content)
-
 		self.line_items = self.tree.xpath('//td[@class="pl "]/a/text()')
 		self.values = self.tree.xpath('//td[@class="nump"]/text()')
 
+		self.format_values()
 		self.mappedData = self.mapData()
 
 	def mapData(self):
-		return dict(zip(self.line_items,self.values))
-
-	def cleanValues(self):
+		pass
+	def format_values(self):
+		"""makes sure the values elements of raw data are all valid integers 
+		except per share amounts, which are valid"""
+		self.values = [x.strip('$ ') for x in self.values  if x != '\n']
+		self.values = [int(x.replace(',', '')) for x in self.values[:2]]
