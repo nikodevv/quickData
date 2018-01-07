5aea8c9fa35872c4dc78dbe6888460356da70cd4
Niko Oshinov <niko.oshinov@gmail.com>
2018-01-06 20:44:04 -0500

    Passing first test

--- dataCreator.py
+++ dataCreator.py
@@ -3,14 +3,14 @@ import requests
 
 class DataScraper():
 	"""Scraps income statement of a given company, aligning historical data"""
-	def __init__(self,company_IS_data_link=):
-		main(company_IS_data_link)
+	def __init__(self,company_IS_data_link):
+		self.main(company_IS_data_link)
 
 	def main(self,company_IS_data_link):
 		self.page = requests.get(company_IS_data_link)
 		self.tree = fromstring(self.page.content)
 
-		self.line_items = tree.xpath('//td[@class="pl "]/a//text()')
+		self.line_items = self.tree.xpath('//td[@class="pl "]/a//text()')
 		self.line_items.pop(0) # unnecessary item which ruins mapping
-		self.values = tree.xpath('//td[@class="nump"]/text()')
+		self.values = self.tree.xpath('//td[@class="nump"]/text()')
 		
--- unittests.py
+++ unittests.py
@@ -3,11 +3,12 @@ from dataCreator import DataScraper
 
 class TestDataCreation(unittest.TestCase):
 	"""Tests scrapping of EDGAR website"""
+
 	def setUp(self):
-		testScraper = DataScraper('https://www.sec.gov/Archives/edgar/data/'
+		self.testScraper = DataScraper('https://www.sec.gov/Archives/edgar/data/'
 			+ '1564408/000156459017022434/R4.htm') # Snapchat Sept 30 2017 10-Q
 
 	def test_can_pull_data_from_link(self):
-		self.assertTrue(testScrapper.line_items)
-		self.assertTrue(testScrapper.values)
-		self.assertContains(testScrapper.line_items[-1],'earnings')
+		self.assertTrue(self.testScraper.line_items)
+		self.assertTrue(self.testScraper.values)
+		self.assertIn(self.testScraper.line_items[-1],'Diluted')
