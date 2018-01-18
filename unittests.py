dc0ecbbfcc01c4e1f409253a5d24493abddd4c71
Niko Oshinov <niko.oshinov@gmail.com>
2018-01-17 19:30:11 -0500

    Added documentation for API

--- unittests.py
+++ unittests.py
@@ -132,12 +132,13 @@ class TestDataCreation(TestCase):
 		self.assertEqual(f_q['period_ended'], 'Q3')
 
 class TestFilings(TestCase):
+
 	def setUp(self):
 		self.testFilings = Filings('1564408') # Snapchat cik
 
 	def test_returns_dict(self):
 		self.assertIsInstance(self.testFilings.raw_data, dict)
-		print(self.testFilings.raw_data[])
+		print(self.testFilings.raw_data['2017Q1'])
 
 	def test_set_latest_period(self):
 		self.testFilings.set_latest_period({'period1' :{'some':'object', 
@@ -154,6 +155,7 @@ class TestFilings(TestCase):
 			self.testFilings.get_row_labels(self.testFilings.raw_data,
 				self.testFilings.statement_keys[1])[0],
 			str)
+
 	@skip
 	def test_picks_correct_compilation_function(self):
 		self.fail("incomplete test")
